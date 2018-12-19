a20fb41f20f5d866cb641915df1a0a61636e9599
grel4d <greer.relphorde@gmail.com>
2018-12-19 06:19:39 +0000

    validation 3

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
+  validates_length_of :name, :minimum =>3 
   validates :address , presence: true
   validates :description, presence: true
 end
