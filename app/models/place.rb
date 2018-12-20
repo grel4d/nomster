af10f79254b20eb1fe5bf270fc33188c2f676193
grel4d <greer.relphorde@gmail.com>
2018-12-20 23:19:46 +0000

    geogode pt1

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,9 @@
 class Place < ApplicationRecord
   belongs_to :user
+
+  geocoded_by :address
+  after_validation :geocode
+
   validates :name, presence: true
   validates_length_of :name, :minimum =>3 
   validates :address , presence: true
